7ae63d2740ae2e1e87e54c648a0aa6ddce1ad7a1
root <root@brahgah.com>
2016-04-27 12:30:20 -0400

    first add commit

---
+++ app/app.component.js
@@ -0,0 +1,3 @@
+(function(app) {
+    app.AppComponent = ng.core.Component({ selector: 'my-app', template: '<ul><li>My First Angular 2 App</li><li>item 2</li></ul>' }).Class({constructor: function() {}});
+})(window.app || (window.app = {}));
